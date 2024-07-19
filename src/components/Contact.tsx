3f3da2f427ec19946913865d6bbb9320986c616e
samlaconico <99830026+samlaconico@users.noreply.github.com>
2024-07-19 16:11:02 -0700

    fixed bottom padding on contact page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,9 +3,9 @@ import ContactForm from "./ContactForm";
 export default function ContactComponent() {
   return (
     <>
-      <div className="md:pb-20">
+      <div className="">
         <h1 className="pt-48 md:pt-0 text-5xl text-center font-sans py-4">
-          ABOUT US
+          CONTACT
         </h1>
 
         <div className="w-screen md:grid md:grid-cols-2 space-x-8 pt-4">
