54c2215fdde821e75e7b9bebc88edfa362e84bc0
samlaconico <99830026+samlaconico@users.noreply.github.com>
2024-07-29 13:11:47 -0700

    changes

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,14 @@
+import BlogCard from "@/components/BlogCard";
+
+export default function Blog() {
+  return (
+    <>
+      <div>
+        <h1 className="pt-48 md:pt-0 text-5xl text-center font-bold font-poppinsBold py-4">
+        BUILDS, BLOGS, AND EVENTS
+        </h1>
+
+      </div>
+    </>
+  );
+}
