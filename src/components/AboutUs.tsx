7b9cc35c2ae200b7890bdf90662445d91aa7ea43
samlaconico <99830026+samlaconico@users.noreply.github.com>
2024-08-02 17:00:40 -0700

    fixed about us on mobile

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -14,7 +14,7 @@ export default function AboutUsComponent() {
             className="m-auto w-full h-full object-cover"
           />
         </div>
-        <div className="py-10 md:py-0 md:pt-8 container text-center pr-10 text-2xl font-jost m-auto">
+        <div className="py-10 md:py-0 md:pt-8 container text-center px-6 text-2xl font-jost m-auto">
           <p>{AboutUsData.description}</p>
         </div>
       </div>
